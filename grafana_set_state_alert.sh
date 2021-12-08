1d8dfb8f561ab27c6407d46c1e13417d519ca6ff
Joris VIAL <58552719+vialjoris@users.noreply.github.com>
2021-12-08 10:18:16 +0100

    fix : change state for all alert in dashboard

--- grafana_set_state_alert.sh
+++ grafana_set_state_alert.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # ./grafana_set_state_alert.sh [ID_DASHBOARD] [pause/play]
 
+# sudo apt install jq
+
+date
+
 ID_DASHBOARD="$1"
 URL_GRAFANA="grafana.url.com"
 CLE_API="grafana_api_key"
@@ -9,22 +13,24 @@ function usage(){
     echo "usage : set_state_alert.sh [ID_DASHBOARD] [pause/play]"
 } 
 
-# get_id_alert 
-function get_id_alert(){
-    CURL_ID_ALERT=$(curl -k --location --silent --request GET "https://${URL_GRAFANA}/api/alerts?dashboardId=${ID_DASHBOARD}" --header 'Accept: application/json' --header 'Content-Type: application/json' --header "Authorization: Bearer ${CLE_API}")
-    ID_ALERT=$( echo ${CURL_ID_ALERT} | for i in * ; do awk -F ':' '{print $2}';done | for i in * ; do awk -F ',' '{print $1}';done )   
+function get_id_alert(){    
+    curl -k --location --request GET "https://${URL_GRAFANA}/api/alerts?dashboardId=${ID_DASHBOARD}" --header 'Accept: application/json' --header 'Content-Type: application/json' --header "Authorization: Bearer ${CLE_API}" | jq -r '.[] | [.id] | @csv '
 }
 
+# set_pause_alert [ID_ALERT]
 function set_pause_alert(){
-    curl -k --location --request POST "https://${URL_GRAFANA}/api/alerts/${ID_ALERT}/pause" --header 'Accept: application/json' --header 'Content-Type: application/json' --header "Authorization: Bearer ${CLE_API}" --data '{ "paused": true }'
+    ID_ALERT=$1
+    curl -ksH --location --request POST "https://${URL_GRAFANA}/api/alerts/${ID_ALERT}/pause" --header 'Accept: application/json' --header 'Content-Type: application/json' --header "Authorization: Bearer ${CLE_API}" --data '{ "paused": true }'
 }
 
+# set_play_alert [ID_ALERT]
 function set_play_alert(){
+    ID_ALERT=$1
     curl -k --location --request POST "https://${URL_GRAFANA}/api/alerts/${ID_ALERT}/pause" --header 'Accept: application/json' --header 'Content-Type: application/json' --header "Authorization: Bearer ${CLE_API}" --data '{ "paused": false }'
 }
 
 case $2 in
-    pause) get_id_alert && set_pause_alert ;;
-     play) get_id_alert && set_play_alert  ;;
+    pause) get_id_alert | while read -r line; do set_pause_alert "$line"; done 2>&1; printf "\n";;
+     play) get_id_alert | while read -r line; do set_play_alert "$line"; done 2>&1; printf "\n"  ;;
         *) usage ;;
 esac
